3278347df7c0963d3b6873f7e14edfdb07264734
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-24 09:47:50 +0530

    Create login.java

---
+++ JTA-Banking-System/src/main/java/lk/jiat/ee/bank/servlet/login.java
@@ -0,0 +1,31 @@
+package lk.jiat.ee.bank.servlet;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lk.jiat.ee.bank.ejb.remote.LoginService;
+
+import java.io.IOException;
+
+@WebServlet(name = "/login")
+public class login extends HttpServlet {
+    @EJB
+    private LoginService loginService;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email =request.getParameter("email");
+        String password =request.getParameter("password");
+      boolean login =  loginService.loginUser(email, password);
+      if (login) {
+          HttpSession session = request.getSession();
+          session.setAttribute("user", email);
+          response.sendRedirect("home.jsp");
+      }else {
+          response.sendRedirect("login.jsp");
+      }
+    }
+}
