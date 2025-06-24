404467d200e5b1e680201808f0f873fcbabe3c1b
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-24 09:48:19 +0530

    Create register.java

---
+++ JTA-Banking-System/src/main/java/lk/jiat/ee/bank/servlet/register.java
@@ -0,0 +1,26 @@
+package lk.jiat.ee.bank.servlet;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ee.bank.ejb.remote.RegisterService;
+
+import java.io.IOException;
+
+@WebServlet("/register")
+public class register extends HttpServlet {
+
+    @EJB
+    private RegisterService registerService;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         String name =request.getParameter("name");
+         String email =request.getParameter("email");
+         String password =request.getParameter("password");
+
+         registerService.registerUser(name, email, password);
+    }
+
+}
