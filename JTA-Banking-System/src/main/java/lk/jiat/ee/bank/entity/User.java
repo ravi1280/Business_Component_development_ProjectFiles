5bf5236a3d7e21fa313fcb9ee250c63233d984ff
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-24 09:47:44 +0530

    Create User.java

---
+++ JTA-Banking-System/src/main/java/lk/jiat/ee/bank/entity/User.java
@@ -0,0 +1,79 @@
+package lk.jiat.ee.bank.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@NamedQueries({
+        @NamedQuery(name = "User.findByEmail",query = "SELECT u FROM User u WHERE u.email=:email"),
+        @NamedQuery(name = "User.findByEmailAndPassword",query = "SELECT u FROM User u WHERE u.email=:email AND u.password=:password"),
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String password;
+    private String email;
+
+    @OneToMany(mappedBy ="user" )
+    private List<Account> accounts = new ArrayList<Account>();
+
+
+    public User( String name, String email ,String password) {
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    public User(){}
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+
+
+}
