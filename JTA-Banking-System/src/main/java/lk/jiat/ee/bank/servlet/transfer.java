58d98ab9101f40ac15059ac207463db6227d1684
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-25 17:18:23 +0530

    Create transfer.java

---
+++ JTA-Banking-System/src/main/java/lk/jiat/ee/bank/servlet/transfer.java
@@ -0,0 +1,28 @@
+package lk.jiat.ee.bank.servlet;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ee.bank.ejb.remote.TransferService;
+
+import java.io.IOException;
+
+@WebServlet("/transfer")
+public class transfer extends HttpServlet {
+    @EJB
+    private TransferService transferService;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       String sourceAccountNumber= request.getParameter("sourceAccountNumber");
+        String destinationAccountNumber= request.getParameter("destinationAccountNumber");
+        String amount = request.getParameter("amount");
+
+        transferService.transferAmount(sourceAccountNumber, destinationAccountNumber, Double.parseDouble(amount));
+
+
+    }
+}
