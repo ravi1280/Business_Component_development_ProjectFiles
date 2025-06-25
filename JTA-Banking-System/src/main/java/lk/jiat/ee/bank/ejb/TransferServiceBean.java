8718fa32aaec393c9114a572820ebb269da47f56
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-25 23:29:18 +0530

    Update TransferServiceBean.java

--- JTA-Banking-System/src/main/java/lk/jiat/ee/bank/ejb/TransferServiceBean.java
+++ JTA-Banking-System/src/main/java/lk/jiat/ee/bank/ejb/TransferServiceBean.java
@@ -1,25 +1,43 @@
 package lk.jiat.ee.bank.ejb;
 
-import jakarta.ejb.EJB;
-import jakarta.ejb.Stateless;
-import jakarta.ejb.TransactionAttribute;
-import jakarta.ejb.TransactionAttributeType;
+import jakarta.ejb.*;
+import jakarta.inject.Inject;
+import jakarta.transaction.*;
 import lk.jiat.ee.bank.ejb.remote.AccountService;
 import lk.jiat.ee.bank.ejb.remote.TransferService;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class TransferServiceBean implements TransferService {
-
     @EJB
     private AccountService accountService;
 
+    @Inject
+    private UserTransaction userTransaction;
     @Override
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+//    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public void transferAmount(String sourceAccountNumber, String destinationAccountNumber, double amount) {
-
-        accountService.debitFromAccount(sourceAccountNumber, amount);
-        accountService.creditToAccount(destinationAccountNumber, amount);
-
+        try {
+            userTransaction.begin();
+            accountService.debitFromAccount(sourceAccountNumber, amount);
+            accountService.creditToAccount(destinationAccountNumber, amount);
+            userTransaction.commit();
+        } catch (NotSupportedException e) {
+            try {
+                userTransaction.rollback();
+            } catch (SystemException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        } catch (SystemException e) {
+            throw new RuntimeException(e);
+        } catch (HeuristicRollbackException e) {
+            throw new RuntimeException(e);
+        } catch (HeuristicMixedException e) {
+            throw new RuntimeException(e);
+        } catch (RollbackException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
