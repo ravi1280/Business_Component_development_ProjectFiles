294bb3587fed2a370bfff84b948bc970ca77e3f6
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-25 17:18:16 +0530

    Update Account.java

--- JTA-Banking-System/src/main/java/lk/jiat/ee/bank/entity/Account.java
+++ JTA-Banking-System/src/main/java/lk/jiat/ee/bank/entity/Account.java
@@ -3,11 +3,48 @@ package lk.jiat.ee.bank.entity;
 import jakarta.persistence.*;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "Account.findByAccountNo", query = "SELECT a FROM Account a WHERE a.accountNo=:accountNumber")
+
+})
 
 public class Account {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(unique=true,nullable=false)
     private String accountNo;
     private double balance;
     @ManyToOne(cascade = CascadeType.ALL)
