989fd9ffe91034d6db24faa45e0878ef4aef129a
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-26 15:45:31 +0530

    Update BeanA.java

--- JTA-Banking-System/src/main/java/lk/jiat/ee/bank/ejb/BeanA.java
+++ JTA-Banking-System/src/main/java/lk/jiat/ee/bank/ejb/BeanA.java
@@ -3,18 +3,32 @@ package lk.jiat.ee.bank.ejb;
 import jakarta.annotation.Resource;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
+import jakarta.ejb.TransactionManagement;
+import jakarta.ejb.TransactionManagementType;
+import jakarta.inject.Inject;
 import jakarta.transaction.TransactionSynchronizationRegistry;
+import jakarta.transaction.UserTransaction;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class BeanA {
     @EJB
     private BeanB beanB;
 
-
+    @Resource
+    private UserTransaction userTransaction;
 
     public void doAction(){
         System.out.println("BeanA doAction");
-
+        System.out.println(userTransaction);
         beanB.doAction();
+        doWork();
     }
+    public void doWork(){
+        System.out.println("BeanA doWork"+userTransaction);
+    }
+    public void doWork1(){
+        System.out.println("BeanA doWork1"+userTransaction);
+    }
+
 }
