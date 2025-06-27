4924f85478c9ade8f91c1437b7c4f638f4844367
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-27 22:44:34 +0530

    Create Test.java

---
+++ J2EE-Timer/src/main/java/lk/jiat/ee/timer/servlert/Test.java
@@ -0,0 +1,41 @@
+package lk.jiat.ee.timer.servlert;
+
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ee.timer.ejb.TaskSessionBean;
+import lk.jiat.ee.timer.ejb.TimerSessionBean;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+    @EJB
+    private TaskSessionBean sessionBean;
+
+    @EJB
+    private TimerSessionBean timerBean;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+//       Future<String> doTask =sessionBean.doTask();
+//        try {
+//           String s = doTask.get();
+//           response.getWriter().println(s);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        } catch (ExecutionException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        timerBean.doTask();
+    }
+}
