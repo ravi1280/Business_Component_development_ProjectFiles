a5d65eb9920151ea445a350b33464cf92cad3ed3
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-29 01:18:29 +0530

    Update TestInterceptor.java

--- J2EE-Interceptors/src/main/java/lk/jiat/ee/interceptor/interceptors/TestInterceptor.java
+++ J2EE-Interceptors/src/main/java/lk/jiat/ee/interceptor/interceptors/TestInterceptor.java
@@ -1,13 +1,29 @@
 package lk.jiat.ee.interceptor.interceptors;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.interceptor.AroundConstruct;
 import jakarta.interceptor.AroundInvoke;
 import jakarta.interceptor.InvocationContext;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Map;
 
 
 public class TestInterceptor {
+    @AroundConstruct
+    public void aroundConstruct(InvocationContext ctx)  {
+        System.out.println("TestConstructor - AroundConstruct"+ctx.getConstructor());
+
+    }
+    @PostConstruct
+    public void init(InvocationContext ic) {
+        System.out.println("TestInterceptor init");
+        Constructor<?> constructor= ic.getConstructor();
+        System.out.println("TestIntercept constructor : " + constructor);
+    }
     @AroundInvoke
     public Object intercept(InvocationContext ic) throws Exception {
         System.out.println("TestIntercept called intercept method");
@@ -19,11 +35,27 @@ public class TestInterceptor {
         //modify parameters before invoking the method
         Object[] parameters = ic.getParameters();
         System.out.println(Arrays.toString(parameters));
-
         ic.setParameters(new Object[]{"Indraji"});
+
+       //we can get  sessionBean Object
+        Object target = ic.getTarget();
+        System.out.println("TestIntercept  target : " + target);
+
+        // we can get Context Data and Add
+        Map<String, Object> contextData = ic.getContextData();
+        System.out.println("TestIntercept contextData : " + contextData);
+        contextData.put("Age", "25");
+
+
+
 
 
         ic.proceed();
         return null;
+    }
+
+    @PreDestroy
+    public void destroy(InvocationContext ic) {
+        System.out.println("TestInterceptor destroy");
     }
 }
