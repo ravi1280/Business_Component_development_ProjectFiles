a4f577e08fe9f684318c895cee48cce331ed90ec
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-29 01:18:21 +0530

    Update UserSessionBean.java

--- J2EE-Interceptors/src/main/java/lk/jiat/ee/interceptor/ejb/UserSessionBean.java
+++ J2EE-Interceptors/src/main/java/lk/jiat/ee/interceptor/ejb/UserSessionBean.java
@@ -3,10 +3,11 @@ package lk.jiat.ee.interceptor.ejb;
 
 import jakarta.ejb.Stateless;
 import jakarta.interceptor.Interceptors;
+import lk.jiat.ee.interceptor.interceptors.AInterceptor;
 import lk.jiat.ee.interceptor.interceptors.TestInterceptor;
 
 @Stateless
-@Interceptors({TestInterceptor.class,})
+@Interceptors({TestInterceptor.class, AInterceptor.class})
 public class UserSessionBean {
     public void doAction(String name) {
         System.out.println("UserSessionBean doAction called : "+name);
