d9719bf02c3139ba3033370d6513fe7fcfbdb15c
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-29 00:24:59 +0530

    Update TestInterceptor.java

--- J2EE-Interceptors/src/main/java/lk/jiat/ee/interceptor/interceptors/TestInterceptor.java
+++ J2EE-Interceptors/src/main/java/lk/jiat/ee/interceptor/interceptors/TestInterceptor.java
@@ -1,13 +1,29 @@
 package lk.jiat.ee.interceptor.interceptors;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.interceptor.AroundInvoke;
+import jakarta.interceptor.InvocationContext;
 
-import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 
 public class TestInterceptor {
-    public void intercept() {
+    @AroundInvoke
+    public Object intercept(InvocationContext ic) throws Exception {
         System.out.println("TestIntercept called intercept method");
+
+        // we can get which method is being invoked
+        Method method = ic.getMethod();
+        System.out.println("TestIntercept called intercept method" + method);
+
+        //modify parameters before invoking the method
+        Object[] parameters = ic.getParameters();
+        System.out.println(Arrays.toString(parameters));
+
+        ic.setParameters(new Object[]{"Indraji"});
+
+
+        ic.proceed();
+        return null;
     }
 }
