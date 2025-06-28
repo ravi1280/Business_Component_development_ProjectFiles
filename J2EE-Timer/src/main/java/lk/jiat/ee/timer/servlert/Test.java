152e884562715bd06f14fdb4695828edb10c4b08
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-28 13:01:49 +0530

    Update Test.java

--- J2EE-Timer/src/main/java/lk/jiat/ee/timer/servlert/Test.java
+++ J2EE-Timer/src/main/java/lk/jiat/ee/timer/servlert/Test.java
@@ -7,12 +7,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ee.timer.TimerPackage.Task;
 import lk.jiat.ee.timer.ejb.TaskSessionBean;
 import lk.jiat.ee.timer.ejb.TimerSessionBean;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 @WebServlet("/test")
 public class Test extends HttpServlet {
@@ -36,6 +38,8 @@ public class Test extends HttpServlet {
 //            throw new RuntimeException(e);
 //        }
 
-        timerBean.doTask();
+       Task task = timerBean.doTask(10000);
+        System.out.println(" Task Name : " +task.getTaskName()+" ,  Task ID :" + task.getTaskId() );
+        request.getSession().setAttribute("task", task);
     }
 }
