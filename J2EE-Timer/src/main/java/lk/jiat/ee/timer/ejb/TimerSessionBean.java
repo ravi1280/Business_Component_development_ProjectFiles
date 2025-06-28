a93552b83d424a2dd495f8a0f1a9cfd89ec999ad
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-28 13:01:41 +0530

    Update TimerSessionBean.java

--- J2EE-Timer/src/main/java/lk/jiat/ee/timer/ejb/TimerSessionBean.java
+++ J2EE-Timer/src/main/java/lk/jiat/ee/timer/ejb/TimerSessionBean.java
@@ -2,6 +2,10 @@ package lk.jiat.ee.timer.ejb;
 
 import jakarta.annotation.Resource;
 import jakarta.ejb.*;
+import lk.jiat.ee.timer.TimerPackage.Task;
+
+import java.io.Serializable;
+import java.util.UUID;
 
 @Singleton
 public class TimerSessionBean {
@@ -11,20 +15,44 @@ public class TimerSessionBean {
 
 //    Timer timer;
 
-    public void doTask() {
+    public Task doTask(long time) {
 //        timerService.createIntervalTimer(1000,5000,new TimerConfig());
 //       timer = timerService.createTimer(60000,"Clock");
-        timerService.createTimer(60000, "Clock");
+
+
+        TimerConfig timerConfig  =new TimerConfig();
+        String taskId =UUID.randomUUID().toString();
+        Task task = new Task(taskId,"Test Task");
+        timerConfig.setInfo(task);
+
+       ScheduleExpression scheduleExpression = new ScheduleExpression();
+        timerService.createCalendarTimer(scheduleExpression, timerConfig);
+
+//        timerService.createSingleActionTimer(time, timerConfig);
+        return task;
     }
 
     @Timeout
     public void timeOutTask(Timer timer) {
-        System.out.println("OK"+timer);
+        Serializable info = timer.getInfo();
+        if (info instanceof Task) {
+            Task task = (Task) info;
+            System.out.println(task.getTaskName() + " " + task.getTaskId() + "  Task Is Done");
+
+        }
     }
 
-    public void cancelTimer(){
-//        if(timer != null){
-//            timer.cancel();
-//        }
+    public void cancelTimer(String taskId) {
+
+        //getAllTimers for get all beans timers
+        //getTimers can get access in bean timers
+        for (Timer timer : timerService.getAllTimers()) {
+            if(timer.getInfo() instanceof Task && ((Task) timer.getInfo()).getTaskId().equals(taskId) ){
+               timer.cancel();
+                System.out.println(" Task Name : " +taskId+"  Task ID :" + taskId +" is cancelled");
+               break;
+            }
+        }
+
     }
 }
