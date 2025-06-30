e596585440ec7c88721192ba0ad38b134192e25f
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-30 20:20:18 +0530

    Create RESTApplicationPath.java

---
+++ J2EE-REST/src/main/java/lk/jiat/ee/rest/RESTApplicationPath.java
@@ -0,0 +1,10 @@
+package lk.jiat.ee.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("/api/v1")
+public class RESTApplicationPath  extends Application {
+
+}
