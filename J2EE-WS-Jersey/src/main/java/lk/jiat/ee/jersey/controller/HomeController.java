4e3efae208729255b592af9056fe8f0de2cc540a
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-02 00:37:51 +0530

    Update HomeController.java

--- J2EE-WS-Jersey/src/main/java/lk/jiat/ee/jersey/controller/HomeController.java
+++ J2EE-WS-Jersey/src/main/java/lk/jiat/ee/jersey/controller/HomeController.java
@@ -3,14 +3,20 @@ package lk.jiat.ee.jersey.controller;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import org.glassfish.jersey.server.mvc.Viewable;
+import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
 
-@Path("/home")
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/")
 public class HomeController {
 
     @GET
-    public String index() {
-        return "Index ";
+    public Viewable index() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("message", "Hello World");
 
+        return new Viewable("/index", model);
     }
-
 }
