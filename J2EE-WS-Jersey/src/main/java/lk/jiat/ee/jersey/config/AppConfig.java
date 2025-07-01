060f56164e9dd7944af6644403d92029575736b5
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-02 00:37:26 +0530

    Update AppConfig.java

--- J2EE-WS-Jersey/src/main/java/lk/jiat/ee/jersey/config/AppConfig.java
+++ J2EE-WS-Jersey/src/main/java/lk/jiat/ee/jersey/config/AppConfig.java
@@ -2,12 +2,16 @@ package lk.jiat.ee.jersey.config;
 
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
 
 
-@ApplicationPath("/api/v1")
+//@ApplicationPath("/api/v1")
 public class AppConfig extends ResourceConfig {
     public AppConfig() {
         packages("lk.jiat.ee.jersey.controller");
         packages("lk.jiat.ee.jersey.middleware");
+
+        register(JspMvcFeature.class);
+        property(JspMvcFeature.TEMPLATE_BASE_PATH, "/WEB-INF/views/");
     }
 }
