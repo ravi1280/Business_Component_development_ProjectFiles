80ee97a184979a8a28b0c35dd98066ef881e0971
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-01 21:03:09 +0530

    Create User.java

---
+++ J2EE-REST/src/main/java/lk/jiat/ee/rest/User.java
@@ -0,0 +1,31 @@
+package lk.jiat.ee.rest;
+
+public class User {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private Integer id;
+    private String name;
+    private String email;
+}
