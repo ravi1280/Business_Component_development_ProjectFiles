5cb13fd5509741681bc23fc6230642af4c2c8de6
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-04 02:15:11 +0530

    Update AppIdentityStore.java

--- J2EE-Security-With-DB/src/main/java/lk/jiat/ee/security/security/AppIdentityStore.java
+++ J2EE-Security-With-DB/src/main/java/lk/jiat/ee/security/security/AppIdentityStore.java
@@ -1,20 +1,38 @@
 package lk.jiat.ee.security.security;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.security.enterprise.credential.Credential;
+import jakarta.security.enterprise.credential.UsernamePasswordCredential;
 import jakarta.security.enterprise.identitystore.CredentialValidationResult;
 import jakarta.security.enterprise.identitystore.IdentityStore;
+import lk.jiat.ee.security.service.LoginService;
+
+import java.util.Set;
 
 
 @ApplicationScoped
 public class AppIdentityStore implements IdentityStore {
-    @PersistenceContext
-    private EntityManager em;
+
+    @Inject
+    private LoginService loginService;
 
     @Override
     public CredentialValidationResult validate(Credential credential) {
-        return IdentityStore.super.validate(credential);
+
+        if(credential instanceof UsernamePasswordCredential){
+            UsernamePasswordCredential upc = (UsernamePasswordCredential)credential;
+          boolean validate =  loginService.validateUser(upc.getCaller(), upc.getPasswordAsString());
+          if(validate){
+             Set<String> roles = loginService.getRoles(upc.getCaller());
+             return new  CredentialValidationResult(upc.getCaller(),roles);
+
+          }
+
+
+        }
+        return CredentialValidationResult.INVALID_RESULT;
     }
 }
