77fda0fdd911deba16b0c6b837ea2f031704305d
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-05 18:30:29 +0530

    Create AppConfig.java

---
+++ J2EE-Security-JWT-With-DB/src/main/java/lk/jiat/ee/security/config/AppConfig.java
@@ -0,0 +1,9 @@
+package lk.jiat.ee.security.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("/api")
+public class AppConfig extends Application {
+}
