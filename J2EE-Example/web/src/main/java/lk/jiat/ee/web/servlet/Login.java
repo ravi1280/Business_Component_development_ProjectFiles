0fd0660edf63ae9a76b18f15e4c3ca31e76d8f42
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-06 19:19:58 +0530

    Update Login.java

--- J2EE-Example/web/src/main/java/lk/jiat/ee/web/servlet/Login.java
+++ J2EE-Example/web/src/main/java/lk/jiat/ee/web/servlet/Login.java
@@ -1,18 +1,49 @@
 package lk.jiat.ee.web.servlet;
 
+import jakarta.ejb.EJB;
+import jakarta.inject.Inject;
+import jakarta.security.enterprise.AuthenticationStatus;
+
+import jakarta.security.enterprise.SecurityContext;
+import jakarta.security.enterprise.authentication.mechanism.http.AuthenticationParameters;
+import jakarta.security.enterprise.credential.UsernamePasswordCredential;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ee.core.service.UserService;
 
 import java.io.IOException;
 
 
 @WebServlet("/login")
 public class Login extends HttpServlet {
+
+
+    @Inject
+    private SecurityContext securityContext;
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        System.out.println("Email: " + email+" Password: " + password);
+
+        AuthenticationParameters parameters = AuthenticationParameters.withParams()
+                .credential(new UsernamePasswordCredential(email, password));
+
+        System.out.println("parameters: " + parameters);
+
+        AuthenticationStatus status = securityContext.authenticate(request, response, parameters);
+        System.out.println("Authentication status: " + status);
+
+        if (status == AuthenticationStatus.SUCCESS) {
+            response.sendRedirect(request.getContextPath() + "/index.jsp");
+        } else {
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
+        }
 
     }
 }
