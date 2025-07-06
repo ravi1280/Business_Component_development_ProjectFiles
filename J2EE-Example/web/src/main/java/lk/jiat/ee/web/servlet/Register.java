da576ac03cc4cab2923abe690feb86b3c89e107a
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-06 19:19:45 +0530

    Update Register.java

--- J2EE-Example/web/src/main/java/lk/jiat/ee/web/servlet/Register.java
+++ J2EE-Example/web/src/main/java/lk/jiat/ee/web/servlet/Register.java
@@ -1,18 +1,33 @@
 package lk.jiat.ee.web.servlet;
 
+import jakarta.ejb.EJB;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ee.core.model.User;
+import lk.jiat.ee.core.service.UserService;
 
 import java.io.IOException;
 
 
 @WebServlet("/register")
 public class Register extends HttpServlet {
+
+    @EJB
+   private UserService userService;
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String contact = request.getParameter("contact");
+
+      User user = new User(name,contact, email, password);
+
+        userService.createUser(user);
 
     }
 }
