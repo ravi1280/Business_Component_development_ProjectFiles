cd0ad75a107f395fbd0a7d6206948beaee31a319
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-07 14:47:24 +0530

    Update Register.java

--- J2EE-Example/web/src/main/java/lk/jiat/ee/web/servlet/Register.java
+++ J2EE-Example/web/src/main/java/lk/jiat/ee/web/servlet/Register.java
@@ -6,11 +6,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ee.core.mail.VerificationMail;
 import lk.jiat.ee.core.model.User;
+import lk.jiat.ee.core.provider.MailServiceProvider;
 import lk.jiat.ee.core.service.UserService;
 import lk.jiat.ee.core.util.Encryption;
 
 import java.io.IOException;
+import java.util.UUID;
 
 
 @WebServlet("/register")
@@ -30,7 +33,15 @@ public class Register extends HttpServlet {
 
         User user = new User(name, contact, email, encryptPassword);
 
+        String verificationCode = UUID.randomUUID().toString();
+        System.out.println(verificationCode);
+        VerificationMail verificationMail = new VerificationMail(email, verificationCode);
+        System.out.println(verificationMail);
+        MailServiceProvider.getInstance().sendMail(verificationMail);
+
         userService.createUser(user);
+
+        response.sendRedirect(request.getContextPath()+"/login.jsp");
 
     }
 }
