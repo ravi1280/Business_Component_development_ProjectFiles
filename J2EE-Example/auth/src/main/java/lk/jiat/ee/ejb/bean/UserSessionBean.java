fde062fd247bdd36e75e8e06a073241f210d7c62
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-07 11:03:45 +0530

    Create UserSessionBean.java

---
+++ J2EE-Example/auth/src/main/java/lk/jiat/ee/ejb/bean/UserSessionBean.java
@@ -0,0 +1,61 @@
+package lk.jiat.ee.ejb.bean;
+
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lk.jiat.ee.core.model.User;
+import lk.jiat.ee.core.service.UserService;
+
+@Stateless
+public class UserSessionBean implements UserService {
+    @PersistenceContext
+    private EntityManager em;
+
+
+    @Override
+    public User getUserByID(long id) {
+       User user = em.find(User.class, id);
+        return user;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+      User  user =  em.createNamedQuery("User.FindByEmail",User.class)
+              .setParameter("email", email)
+              .getSingleResult();
+        return user;
+//        em.createNamedQuery("User.findByEmail").setParameter(1, email);
+    }
+
+    @Override
+    public void createUser(User user) {
+        em.persist(user);
+
+    }
+
+    @RolesAllowed({"USER","ADMIN","SUPER_ADMIN"})
+    @Override
+    public void updateUser(User user) {
+        em.merge(user);
+
+    }
+
+    @RolesAllowed({"USER","ADMIN","SUPER_ADMIN"})
+    @Override
+    public void deleteUser(User user) {
+        em.remove(user);
+
+    }
+
+    @Override
+    public boolean userExists(String email, String password) {
+        User user = em.createNamedQuery("User.FindByEmailAndPassword", User.class)
+                .setParameter("email", email)
+                .setParameter("password", password)
+                .getSingleResult();
+
+        return user != null;
+    }
+}
