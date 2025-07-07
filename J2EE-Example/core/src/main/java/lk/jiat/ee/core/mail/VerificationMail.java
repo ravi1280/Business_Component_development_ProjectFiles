f8e17f2bc8d8dc418884daad33add215ce2b061f
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-08 01:04:23 +0530

    Update VerificationMail.java

--- J2EE-Example/core/src/main/java/lk/jiat/ee/core/mail/VerificationMail.java
+++ J2EE-Example/core/src/main/java/lk/jiat/ee/core/mail/VerificationMail.java
@@ -3,6 +3,9 @@ package lk.jiat.ee.core.mail;
 import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
+import lk.jiat.ee.core.util.Encryption;
+
+import java.util.Base64;
 
 public class VerificationMail extends Mailable {
 
@@ -18,9 +21,11 @@ public class VerificationMail extends Mailable {
     public void build(Message message) throws Exception {
         message.setRecipient(Message.RecipientType.TO,new InternetAddress(to));
         message.setSubject("verification Mail");
+        String encode = Base64.getEncoder().encodeToString(to.getBytes());
+        String link ="http://localhost:8080/J2EE-Example/verify?id="+encode+"&vc="+verificationCode;
 
-        message.setText("Hello Dev, Your verification code is : " + verificationCode);
-        //message.setContent(verificationCode, "text/html; charset=utf-8");
+//        message.setText("Hello Dev, Your verification code is : " + verificationCode);
+        message.setContent(link, "text/html; charset=utf-8");
 
     }
 }
